7b0e4fc351a6b70aafcd9e32cb5d289883e2a86a
shakedbo <shakedbozi@gmail.com>
2022-05-22 00:54:29 +0300

    fix email: true

--- Backend/app/Routes/Middlewares/validateEmail.js
+++ Backend/app/Routes/Middlewares/validateEmail.js
@@ -1,6 +1,6 @@
 const prisma = require("../../prisma/prisma")
 
 module.exports = async function validateEmail(email) {
-    const user = await prisma.user.findUnique({ select: { email }, where: { email: email } })
+    const user = await prisma.user.findUnique({ select: { email:true }, where: { email: email } })
     return !!user
 }
